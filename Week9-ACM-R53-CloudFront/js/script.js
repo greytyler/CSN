7b8c31e4242f1e7bdb467e43d384997c89946f8d
Aubrey T Dube <81259406+greytyler@users.noreply.github.com>
2025-08-09 12:00:07 +0200

    csn9 static site

---
+++ Week9-ACM-R53-CloudFront/js/script.js
@@ -0,0 +1,4 @@
+
+document.addEventListener("DOMContentLoaded", function() {
+    console.log("Week 9 Project Website Loaded Successfully!");
+});
---
+++ Week9-ACM-R53-CloudFront/script.js
@@ -0,0 +1,3 @@
+document.addEventListener("DOMContentLoaded", () => {
+    console.log("Website Loaded: Secure DNS & CloudFront Project");
+});
